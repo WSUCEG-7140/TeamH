718ad48e6d18a68bc285c564669c6058dbe63f65
Nikhitha-wsu <133179620+Nikhitha-wsu@users.noreply.github.com>
2023-07-04 16:38:04 -0400

    Adding classes for creating tables

--- PatientInfoapp/models.py
+++ PatientInfoapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class PatientGeneralInfo(models.Model):
+    firstName = models.CharField(max_length=100)
+    lastName = models.CharField(max_length=100)
+    age = models.IntegerField()
+    gender = models.CharField(max_length=10)
+    contactNumber = models.CharField(max_length=20)
+    emailId = models.EmailField()
+    address = models.CharField(max_length = 200)
+    
+    
