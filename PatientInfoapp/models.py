aeaf567252950116d5babca50f50f03dd8883969
Nikhitha-wsu <133179620+Nikhitha-wsu@users.noreply.github.com>
2023-07-05 14:48:44 -0400

    Creating Patient Health Info table

--- PatientInfoapp/models.py
+++ PatientInfoapp/models.py
@@ -1,19 +1,18 @@
 from django.db import models
 
 # Create your models here.
-class PatientGeneralInfo(models.Model):
-    firstName = models.CharField(max_length=100)
-    lastName = models.CharField(max_length=100)
+class patient_generalinfo(models.Model): 
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
     age = models.IntegerField()
     gender = models.CharField(max_length=10)
-    contactNumber = models.CharField(max_length=20)
-    emailId = models.EmailField()
-    address = models.CharField(max_length = 200)
+    phone_number = models.CharField(max_length=20) 
+    email = models.EmailField()
+    address = models.CharField(max_length=200)
+    
 class HealthInfo(models.Model):
     patient = models.OneToOneField(PatientGeneralInfo, on_delete=models.CASCADE)
     bloodGroup = models.CharField(max_length=20)
     symptoms = models.TextField()
     disease = models.CharField(max_length=50)
     treatment = models.TextField()
-    
-    
