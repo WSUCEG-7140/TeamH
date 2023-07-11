3a833214f523bc7f320558cecff790c376414a09
Prathyusha-WSU <133186177+Prathyusha-WSU@users.noreply.github.com>
2023-07-11 14:18:44 -0400

    updating urls.py

--- PatientInfoapp/urls.py
+++ PatientInfoapp/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("patient/<int:pk>/", views.patient_detail, name="patient_detail"),
-    path("add_patient/", views.add_patient, name="add_patient")
+    path("add_patient/", views.add_patient, name="add_patient"),
+    path("edit_patient/<int:pk>/", views.edit_patient, name="edit_patient"),
+    path("delete_patient/<int:pk>/", views.delete_patient, name="delete_patient"),
 ]
