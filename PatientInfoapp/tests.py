db9796876d699a6097ae21b7f54f282d5951c16b
Sindhura <130100217+Sindhura2303@users.noreply.github.com>
2023-07-11 02:20:33 -0400

    Testing patient_generalinfo

--- PatientInfoapp/tests.py
+++ PatientInfoapp/tests.py
@@ -13,6 +13,17 @@ class PatientAppTestCase(TestCase):
             "email": "kari.sindhu@gmail.com",
             "address": "Province,Cloveridge ct.",
         }
+        self.health_info = {
+            "blood_group": "O+",
+            "height": 175,
+            "weight": 50,
+            "blood_pressure": "120/80",
+            "symptoms": "Fever, cold",
+            "disease": "Common cold",
+            "treatment": "take rest",
+            "diagnosis_date": "2023-07-11",
+            "doctor_name": "Dr. Srikanth",
+        }
         
 
 
