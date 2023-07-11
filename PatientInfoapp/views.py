f146cb44e485ca4fbc8f51805ee0efe5e606f5e3
Nikhitha-wsu <133179620+Nikhitha-wsu@users.noreply.github.com>
2023-07-11 12:17:46 -0400

    Creating endpoint for search and retrieve

--- PatientInfoapp/views.py
+++ PatientInfoapp/views.py
@@ -8,4 +8,17 @@ def patient_detail(request, pk):
     patient = get_object_or_404(patient_generalinfo, pk=pk)
 
     #Render "patientInfoapp/patient_detail.html" template, by passing patient as context
-    return render(request, "patientApp/patient_detail.html", {"patient":patient})
+    return render(request, "PatientInfoapp/patient_detail.html", {"patient":patient})
+
+def home(request):
+    # Retrieve the value of "search_email" parameter from the request's GET parameters
+    search_email = request.GET.get("search_email")
+    # Check if a search_email value is provided
+    if search_email:
+        # If search_email is provided, filter the patient_generalinfo objects based on email containing the search_email value
+        patients = patient_generalinfo.objects.filter(email__icontains=search_email)
+    else:
+        # If search_email is not provided, fetch all patient_generalinfo objects
+        patients = patient_generalinfo.objects.all()
+    # Render the "home.html" template with the patients queryset as context data
+    return render(request, "PatientInfoapp/home.html", {"patients": patients})
