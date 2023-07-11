63305a7104b2ab698e59df219a52069ebc7ecd32
Sindhura <130100217+Sindhura2303@users.noreply.github.com>
2023-07-11 02:00:23 -0400

    Testing patient_generalinfo

--- PatientInfoapp/tests.py
+++ PatientInfoapp/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from .models import patient_generalinfo, patient_healthinfo
 
 # Create your tests here.
+class PatientAppTestCase(TestCase):
+    def setUp(self):
+        self.general_info = {
+            "first_name": "Sindhu",
+            "last_name": "Kari",
+            "age": 21,
+            "gender": "Female",
+            "phone_no": "178463934",
+            "email": "kari.sindhu@gmail.com",
+            "address": "Province,Cloveridge ct.",
+        }
+        
+
+
+
