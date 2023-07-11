c40612be6a80633d19f46aee06fea9882329e35c
spadala-wsu <133179668+spadala-wsu@users.noreply.github.com>
2023-07-11 02:50:36 -0400

    Creating instances

--- PatientInfoapp/tests.py
+++ PatientInfoapp/tests.py
@@ -4,6 +4,7 @@ from .models import patient_generalinfo, patient_healthinfo
 #This class includes all the test methods
 class PatientAppTestCase(TestCase):
     def setUp(self):
+        #Adding test data for patient_generalinfo model 
         self.general_info = {
             "first_name": "Sindhu",
             "last_name": "Kari",
@@ -13,6 +14,7 @@ class PatientAppTestCase(TestCase):
             "email": "kari.sindhu@gmail.com",
             "address": "Province,Cloveridge ct.",
         }
+        #Adding test data for patient_healthinfo model
         self.health_info = {
             "blood_group": "O+",
             "height": 175,
@@ -24,6 +26,12 @@ class PatientAppTestCase(TestCase):
             "diagnosis_date": "2023-07-11",
             "doctor_name": "Dr. Srikanth",
         }
+        #creating an instance for patient_generalinfo model by using above test data
+        self.patient = patient_generalinfo.objects.create(**self.general_info)
+        #linking the patient_generalinfo instance to patient_healthinfo  
+        self.health_info["patient"] = self.patient
+        #creating an instance for patient_healthinfo model by using above test data
+        self.health = patient_healthinfo.objects.create(**self.health_info)
         
 
 
