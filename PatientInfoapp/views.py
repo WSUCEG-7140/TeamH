f3b955df24ab5a646058a332c17134ec140b0a6e
Nikhitha-wsu <133179620+Nikhitha-wsu@users.noreply.github.com>
2023-07-20 21:12:56 -0400

    Added automated code documentation

--- PatientInfoapp/views.py
+++ PatientInfoapp/views.py
@@ -5,10 +5,10 @@ from .forms import GeneralInfoForm, HealthInfoForm
 from django.db.models import Q
 
 def patient_detail(request, pk):
-    #Get the patient_generalinfo instance based on the provided primary key(pk)
+    ##Get the patient_generalinfo instance based on the provided primary key(pk)
     patient = get_object_or_404(patient_generalinfo, pk=pk)
 
-    #Render "patientInfoapp/patient_detail.html" template, by passing patient as context
+    ##Render "patientInfoapp/patient_detail.html" template, by passing patient as context
     return render(request, "PatientInfoapp/patient_detail.html", {"patient":patient})
 
 def home(request):
